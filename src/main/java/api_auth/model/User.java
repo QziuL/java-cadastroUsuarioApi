dc5b28cbf43309e2cbf79c84021c5af393547c88
QziuL <lf.quinholi2@gmail.com>
2024-07-05 16:31:21 -0300

    fix: uuid, propriedade 'is_admin' e construtor

--- src/main/java/api_auth/model/User.java
+++ src/main/java/api_auth/model/User.java
@@ -2,24 +2,37 @@ package api_auth.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(unique = true, nullable = false, length = 255)
-    private String email;
+    private UUID id;
 
     @Column(nullable = false, length = 255)
     private String username;
+
+    @Column(unique = true, nullable = false, length = 255)
+    private String email;
 
     @Column(nullable = false)
     private String password;
 
     @Column(nullable = false)
     private String confirmPassword;
+
+    private boolean is_admin = false;
+
+    public User(String username, String email, String password, String confirmPassword) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
 }
