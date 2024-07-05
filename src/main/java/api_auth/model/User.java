01b9ad066b92738f28da997437f80b65b6634286
QziuL <lf.quinholi2@gmail.com>
2024-07-05 14:17:08 -0300

    feat: user model

--- src/main/java/api_auth/model/User.java
+++ src/main/java/api_auth/model/User.java
@@ -1,9 +1,25 @@
 package api_auth.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false, length = 255)
     private String email;
+
+    @Column(nullable = false, length = 255)
     private String username;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String confirmPassword;
 }
