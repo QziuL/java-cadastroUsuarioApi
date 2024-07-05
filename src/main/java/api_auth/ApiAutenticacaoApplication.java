b5da2ae54d5a698c255fb293c607dff84da2eeaf
QziuL <lf.quinholi2@gmail.com>
2024-07-05 14:21:09 -0300

    docs: referencia do projeto

--- src/main/java/api_auth/ApiAutenticacaoApplication.java
+++ src/main/java/api_auth/ApiAutenticacaoApplication.java
@@ -3,6 +3,19 @@ package api_auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 	Estudo de API para autenticação de usuários.
+ * 	Projeto inicializado com Spring Initializr.
+ * 		Dependências usadas:
+ * 		- Spring Web,
+ * 		- H2 Database,
+ * 		- PostgreSQL Driver,
+ * 		- Spring Security,
+ * 		- Spring Data JPA
+ *
+ * @author --> QziuL (Github)
+ */
+
 @SpringBootApplication
 public class ApiAutenticacaoApplication {
 
