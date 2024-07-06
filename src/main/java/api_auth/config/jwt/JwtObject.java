64cc893a7d1fbb5936134fe622701e3e7ba83ed7
QziuL <lf.quinholi2@gmail.com>
2024-07-06 15:45:48 -0300

    feat: classe JwtObject

---
+++ src/main/java/api_auth/config/jwt/JwtObject.java
@@ -0,0 +1,17 @@
+package api_auth.config.jwt;
+
+import lombok.Data;
+
+import java.util.*;
+
+@Data
+public class JwtObject {
+    private String subject;
+    private Date issuedAt;
+    private Date expiresAt;
+    private List<String> roles;
+
+    public void setRoles(String roles) {
+        this.roles = Collections.singletonList(roles);
+    }
+}
