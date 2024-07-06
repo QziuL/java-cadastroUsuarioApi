83f6288890aa9dbbcfa834f76f0af43bd8439bb8
QziuL <lf.quinholi2@gmail.com>
2024-07-06 15:40:42 -0300

    feat: user controller

---
+++ src/main/java/api_auth/controller/UserController.java
@@ -0,0 +1,29 @@
+package api_auth.controller;
+
+import api_auth.dto.UserDTO;
+import api_auth.model.User;
+import api_auth.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> insertUser(@RequestBody UserDTO userDTO) {
+        User userCreated = userService.insert(userDTO);
+        return new ResponseEntity<>(userCreated, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAllUsers() {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+}
