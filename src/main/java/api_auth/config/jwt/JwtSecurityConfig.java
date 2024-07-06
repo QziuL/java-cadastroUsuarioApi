0bef6feab2704fb0c0d601b64d5b7a5b1aea651e
QziuL <lf.quinholi2@gmail.com>
2024-07-06 15:39:54 -0300

    feat: jwt security config

---
+++ src/main/java/api_auth/config/jwt/JwtSecurityConfig.java
@@ -0,0 +1,14 @@
+package api_auth.config.jwt;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "security.config")
+public class JwtSecurityConfig {
+    public static String PREFIX;
+    public static String TOKEN;
+    public static Long EXPIRATION;
+}
