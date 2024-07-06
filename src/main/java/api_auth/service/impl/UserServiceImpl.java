06964c63a5ea9559a999e31cc0f41f0f2d37d887
QziuL <lf.quinholi2@gmail.com>
2024-07-06 15:45:19 -0300

    feat: métodos insert e findAll

--- src/main/java/api_auth/service/impl/UserServiceImpl.java
+++ src/main/java/api_auth/service/impl/UserServiceImpl.java
@@ -2,25 +2,42 @@ package api_auth.service.impl;
 
 import api_auth.dto.UserDTO;
 import api_auth.model.User;
+import api_auth.repository.UserRepository;
 import api_auth.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
+@Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     @Override
-    public UserDTO insert(UserDTO user) {
-        return null;
+    public User insert(UserDTO userDTO) {
+        if(!userDTO.getPassword().equals(userDTO.getConfirmPassword()))
+            throw new IllegalArgumentException("Passwords do not match");
+        if(userRepository.findByEmail(userDTO.getEmail()).isPresent())
+            throw new IllegalArgumentException("Email already exists");
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        return userDTO.convertToUser();
     }
 
     @Override
-    public List<UserDTO> getAll() {
-        return List.of();
+    public List<User> findAll() {
+        return userRepository.findAll();
     }
 
     @Override
-    public UserDTO getById(UUID id) {
+    public User getById(UUID id) {
         return null;
     }
 
