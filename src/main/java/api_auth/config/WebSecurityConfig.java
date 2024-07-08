36e753041dda5e9b0173e05596c957a0156bd821
QziuL <lf.quinholi2@gmail.com>
2024-07-08 18:13:14 -0300

    feat: rotas e middlewares

--- src/main/java/api_auth/config/WebSecurityConfig.java
+++ src/main/java/api_auth/config/WebSecurityConfig.java
@@ -1,37 +1,77 @@
 package api_auth.config;
 
+import api_auth.config.jwt.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    private static final String[] SWAGGER_WHITELIST = {
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+    };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((req) -> req.requestMatchers("/").permitAll().anyRequest().authenticated())
-                .formLogin((form) -> form.loginPage("/login").permitAll().defaultSuccessUrl("/"))
-                .logout(LogoutConfigurer::permitAll);
+                .headers(h -> h.frameOptions(f -> f.disable()))
+                .cors(c -> c.disable())
+                .csrf(c -> c.disable())
+                .addFilterAfter(new JwtFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers(SWAGGER_WHITELIST).permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/role").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/role").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/user").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/user").hasAnyRole("USER","ADMIN")
+                        .requestMatchers("/admin").hasAnyRole("ADMIN")
+                        .requestMatchers("/").permitAll().anyRequest().authenticated())
+                .logout(LogoutConfigurer::permitAll)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
+    // .formLogin(form -> form.loginPage("/login").permitAll().defaultSuccessUrl("/"))
+
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("admin")
-                        .password("admin")
-                        .roles("MANAGER")
-                        .build();
+        UserDetails user = User.builder()
+                            .username("admin")
+                            .password(encoder().encode("admin001"))
+                            .roles("ADMIN")
+                            .build();
 
         return new InMemoryUserDetailsManager(user);
     }
