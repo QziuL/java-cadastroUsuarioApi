27ae7e298a2c97d2e2706b3fde46b6cbbdc7a825
QziuL <lf.quinholi2@gmail.com>
2024-07-08 18:19:47 -0300

    feat: add roles

--- src/main/java/api_auth/dto/UserDTO.java
+++ src/main/java/api_auth/dto/UserDTO.java
@@ -1,7 +1,10 @@
 package api_auth.dto;
 
+import api_auth.model.Role;
 import api_auth.model.User;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class UserDTO {
@@ -9,9 +12,10 @@ public class UserDTO {
     private String email;
     private String password;
     private String confirmPassword;
+    private List<String> roles;
 
     public User convertToUser() {
-        return new User(this.username, this.email, this.password);
+        return new User(this.username, this.email, this.password, this.roles);
     }
 
     public UserDTO getSelf() {
