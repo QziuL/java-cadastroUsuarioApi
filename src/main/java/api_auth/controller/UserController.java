07300055df92af17a154a695e03865dea1f8c136
QziuL <lf.quinholi2@gmail.com>
2024-07-10 15:10:11 -0300

    feat: novos endpoints

--- src/main/java/api_auth/controller/UserController.java
+++ src/main/java/api_auth/controller/UserController.java
@@ -16,6 +16,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping("/auth")
+    public ResponseEntity<String> authenticate() {
+        return new ResponseEntity<>("Usuário autorizado", HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<User> insertUser(@RequestBody UserDTO userDTO) {
         User userCreated = userService.insert(userDTO);
@@ -25,5 +30,15 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> findAllUsers() {
         return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable("email") String email) {
+        return new ResponseEntity<>(userService.getByEmail(email), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<User> deleteUserByEmail(@PathVariable("email") String email) {
+        return new ResponseEntity<>(userService.deleteByEmail(email), HttpStatus.OK);
     }
 }
